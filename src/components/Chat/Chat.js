141a006f031b73740a9bf2c9d27923a2f84f73a8
Rumatoid <skripnikovanton.r@gmail.com>
2020-02-08 22:41:18 +0300

    Main

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,74 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
+import TextContainer from './TextContainer/TextContainer';
+import Messages from './Messages/Messages';
 import InfoBar from './InfoBar/InfoBar';
 import Input from './Input/Input';
-import Messages from '../Messages/Messages';
-import TextContainer from './TextContainer/TextContainer';
 
 import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:5000';
 
-    const ENDPOINT = 'https://react-dnd-test.herokuapp.com/';
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-    useEffect(() => {
-        const { name, room } = queryString.parse(location.search)
+    socket = io(ENDPOINT);
 
-        socket = io(ENDPOINT)
+    setRoom(room);
+    setName(name)
 
-        setName(name);
-        setRoom(room);
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
 
-        socket.emit('join', { name, room }, () =>{
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message ]);
+    });
 
-        });
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    })
 
-        return () => {
-            socket.emit('disconnect');
+    return () => {
+      socket.emit('disconnect');
 
-            socket.off();
-        }
-    }, [ENDPOINT, location.search]);
-
-    useEffect(() =>{
-        socket.on('message', (message) => {
-            setMessages([...messages, message]);
-        })
-
-        // socket.on('roomData', ({ users }) => {
-        //     setUsers(users);
-        // })
-    }, [messages]);
-
-    const sendMessage = ( event ) => {
-        event.preventDefault();
-
-        if(message){
-            socket.emit('sendMessage', message, () => setMessage(''));
-        }
+      socket.off();
     }
+  }, [messages])
 
-    console.log(message,messages);
-    
-    return(
-        <div className='outerContainer'>
-            <div className='container'>
-            <InfoBar room={room} />
-            <Messages messages={messages} name={name} />
-            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-            </div>
-            <TextContainer users={users}/>
-        </div>
-    )
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+    </div>
+  );
 }
 
 export default Chat;
