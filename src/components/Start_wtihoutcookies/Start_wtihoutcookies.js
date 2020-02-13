0f9b37a8f4ac2f50d43d5e4ba9f0e4870f117538
Rumatoid <skripnikovanton.r@gmail.com>
2020-02-13 07:12:42 +0300

    1

--- src/components/Start_wtihoutcookies/Start_wtihoutcookies.js
+++ src/components/Start_wtihoutcookies/Start_wtihoutcookies.js
@@ -1,25 +1,53 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './Start_wtihoutcookies.css';
 
-
 const Start_wtihoutcookies = () => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
 
-    return(
-        <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
-                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
-                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
-                <Link onClick={event => (!name || !room ) ? event.preventDefault() : null} to={`/`}>
-                    <button className="button mt-20" type="submit">Sign In</button>
-                </Link>
-            </div>      
+  const onSubmit = e => {
+    const post = {
+      title: name,
+      text: room
+    };
+    console.log('lol');
+    axios.post('http://localhost:5000/posts', post);
+  };
+
+  return (
+    <div className='joinOuterContainer'>
+      <div className='joinInnerContainer'>
+        <h1 className='heading'>Join</h1>
+        <div>
+          <input
+            placeholder='Name'
+            className='joinInput'
+            type='text'
+            onChange={event => setName(event.target.value)}
+          />
         </div>
-    )
-}
+        <div>
+          <input
+            placeholder='Room'
+            className='joinInput mt-20'
+            type='text'
+            onChange={event => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={e => (!name || !room ? e.preventDefault() : onSubmit(e))}
+          to={`/`}
+        >
+          <button className='button mt-20' type='submit'>
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Start_wtihoutcookies;
