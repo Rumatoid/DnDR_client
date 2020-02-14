3053fa6819e89ebec47b3709bab94aa1d7bf0ff9
Rumatoid <52918376+Rumatoid@users.noreply.github.com>
2020-02-14 22:27:21 +0300

    1

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -4,21 +4,55 @@ import axios from 'axios';
 
 import './LogIn.css';
 
-const Validator = ({ Valid, text }) => {
+const Validator = ({ Valid, text, style }) => {
   if (!Valid) {
     return <span className='some-form__hint'>{text}</span>;
   }
-  return <span className='some-form__hint'>{text}</span>;
+  return <span className='some-form__hint' style={style}></span>;
 };
 
 const Start_wtihoutcookies = () => {
   const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
 
+  const [style, setStyle] = useState({ zIndex: '-1', opacity: '0' });
+  const [style_nickname, setStyle_nickname] = useState();
+  const [style_password, setStyle_password] = useState();
+
   const [nicknameValid, setNicknameValid] = useState(true);
   const [passwordValid, setPasswordValid] = useState(true);
 
-  const onSubmit = e => {
+  //Refs
+  const inputNicknameRef = React.createRef();
+  const inputPasswordRef = React.createRef();
+
+  useEffect(() => {
+    inputNicknameRef.current.focus();
+  }, [inputNicknameRef]);
+
+  useEffect(() => {
+    const borderRed = {
+      borderColor: '#ff4141',
+      boxShadow:
+        'inset 0 0.5px 10.5px rgba(0, 0, 0, 0.075), 0 0 8px rgba(252, 44, 44, 0.6)'
+    };
+
+    if (!passwordValid) setStyle_password(borderRed);
+    else setStyle_password();
+  }, [passwordValid]);
+
+  useEffect(() => {
+    const borderRed = {
+      borderColor: '#ff4141',
+      boxShadow:
+        'inset 0 0.5px 10.5px rgba(0, 0, 0, 0.075), 0 0 8px rgba(252, 44, 44, 0.6)'
+    };
+
+    if (!nicknameValid) setStyle_nickname(borderRed);
+    else setStyle_nickname();
+  }, [nicknameValid]);
+
+  const onSubmit = () => {
     const post = {
       title: nickname,
       text: password
@@ -28,6 +62,9 @@ const Start_wtihoutcookies = () => {
   };
   const onSubmitErr = e => {
     e.preventDefault();
+
+    if (!nickname) setNicknameValid(false);
+    if (!password) setPasswordValid(false);
   };
 
   const handleNicknameChange = value => {
@@ -42,27 +79,55 @@ const Start_wtihoutcookies = () => {
     else setPasswordValid(false);
   };
 
+  const handlePressEnter_onNickname = key => {
+    if (key.charCode === 13) {
+      if (!nickname) setNicknameValid(false);
+      else inputPasswordRef.current.focus();
+    }
+  };
+
+  const handlePressEnter = key => {
+    if (key.charCode === 13) {
+      if (!password) setPasswordValid(false);
+      else onSubmit();
+    }
+  };
+
   return (
     <div className='joinOuterContainer'>
       <div className='joinInnerContainer'>
         <h1 className='heading'>Join</h1>
         <div className='some-form__line'>
           <input
+            ref={inputNicknameRef}
+            style={style_nickname}
             placeholder='Nickname'
             className='joinInput'
             type='text'
             onChange={event => handleNicknameChange(event.target.value)}
+            onKeyPress={handlePressEnter_onNickname}
           />
-          <Validator Valid={nicknameValid} text='Nickname error' />
+          <Validator
+            Valid={nicknameValid}
+            text='Nickname error'
+            style={style}
+          />
         </div>
         <div className='some-form__line'>
           <input
+            ref={inputPasswordRef}
+            style={style_password}
             placeholder='Password'
             className='joinInput mt-20'
             type='text'
             onChange={event => handlePasswordChange(event.target.value)}
+            onKeyPress={handlePressEnter}
           />
-          <Validator Valid={passwordValid} text='Password error' />
+          <Validator
+            Valid={passwordValid}
+            text='Password error'
+            style={style}
+          />
         </div>
         <Link
           onClick={e => (!nickname || !password ? onSubmitErr(e) : onSubmit(e))}
